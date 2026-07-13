a496856d769b0e9917f00c8093345100e026d018
Yogeshvari Suryawanshi <yogeshvari2022@gmail.com>
2026-07-13 22:57:08 +0530

    Added job service

---
+++ src/modules/jobs/services/job.service.ts
@@ -0,0 +1,22 @@
+import Job from "@/database/models/Job";
+import { CreateJobSchema } from "../schemas/job.schema";
+
+export async function createJob(
+  data: CreateJobSchema,
+  employerId: string
+) {
+  const job = await Job.create({
+    ...data,
+    employer: employerId,
+  });
+
+  return job;
+}
+
+export async function getAllJobs() {
+  return Job.find()
+    .populate("employer", "name email")
+    .sort({
+      createdAt: -1,
+    });
+}
