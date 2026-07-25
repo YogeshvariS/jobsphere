76af5d75ea00000ae14b7a581bd9697e208da55f
Yogeshvari Suryawanshi <yogeshvari2022@gmail.com>
2026-07-25 22:25:32 +0530

    testing form

--- src/modules/auth/components/LoginForm.tsx
+++ src/modules/auth/components/LoginForm.tsx
@@ -1,9 +1,3 @@
-// "use client";
-
-// import { useState } from "react";
-// import { signIn } from "next-auth/react";
-// import { useRouter } from "next/navigation";
-
 // export default function LoginForm() {
 //   const router = useRouter();
 
