f43a1d6afe3634831b3370fc2c9fb6fe4b875480
YogeshvariS <yogeshvari2022@gmail.com>
2026-08-02 22:01:00 +0530

    testing

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -2,7 +2,6 @@ export default function RegisterPage() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1>Register Page</h1>
-      <h3>Testing</h3>
     </main>
   );
 }
