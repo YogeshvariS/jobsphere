5ff304673eb318850c2ceb3d1659ab5a71df2999
YogeshvariS <yogeshvari2022@gmail.com>
2026-08-03 22:45:22 +0530

    updated

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -2,6 +2,7 @@ export default function RegisterPage() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1>Register Page</h1>
+      <h4>Sign up with email or phone</h4>
     </main>
   );
 }
